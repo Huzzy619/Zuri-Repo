b3b335a6ffa35a42cf98bd83b87696a1bdda1163
Tobylobah <bellolateefatoluwatobiloba@gmail.com>
2022-06-14 07:54:41 +0100

    First changes

---
+++ I4G025323ITA/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Return the User model that is active in this project.
+User = get_user_model()
+
+class Post (models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now= True)
+    published_date = models.DateTimeField()
